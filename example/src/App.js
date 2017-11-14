813a22de5b6d22611801af936bfc73ce2c755488
Scott Antipa <scottantipa@gmail.com>
2017-11-14 09:03:51 -0800

    Cleanup example code

--- example/src/App.js
+++ example/src/App.js
@@ -6,8 +6,18 @@ import { MapInteractionCSS } from 'react-map-interaction';
 class App extends Component {
   render() {
     // set container node at an origin other than client 0,0 to make sure we handle this case
+    const offset = 20;
+
+    const style = {
+      position: 'absolute',
+      top: offset,
+      left: offset,
+      width: `calc(100vw - ${offset}px)`,
+      height: `calc(100vh - ${offset}px)`
+    }
+
     return (
-      <div style={{ position: 'absolute', top: 50, left: 25 }}>
+      <div style={style}>
         <MapInteractionCSS minScale={.05} maxScale={5} showControls>
           <img src="/grid.png" style={{ pointerEvents: 'none' }} alt="" />
         </MapInteractionCSS>
