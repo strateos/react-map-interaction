ef0af50a5175be9249783512082497c3d508a791
Scott Antipa <scottantipa@gmail.com>
2018-06-28 15:11:36 -0700

    Fix typo that caused us to only use x dimension to calculate distance between touch points (#19)

--- index.jsx
+++ index.jsx
@@ -22,7 +22,7 @@ const eventNames = () => {
 const distance = (p1, p2) => {
   const dx = p1.x - p2.x;
   const dy = p1.y - p2.y;
-  return Math.sqrt(Math.pow(dx, 2), Math.pow(dy, 2));
+  return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 }
 
 const midpoint = (p1, p2) => {
