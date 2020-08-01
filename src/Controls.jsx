28a096f95282674451527140dc02026b4fe13116
Yash Sukhwani <43533666+YashSukhwani@users.noreply.github.com>
2020-07-31 19:21:04 -0500

    Update Controls.jsx

--- src/Controls.jsx
+++ src/Controls.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-class Controls extends Component {
+class Controls extends PureComponent {
   render() {
     const {
       plusBtnContents,
